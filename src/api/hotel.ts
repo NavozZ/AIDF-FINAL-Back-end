d1c7482557d7597b92a212f6a00736880a02b98b
NavozZ <navodyatheshan4@gmail.com>
2025-11-12 04:56:45 +0530

    Refactor hotel API: streamline router setup, enhance error handling, and improve query filtering

--- src/api/hotel.ts
+++ src/api/hotel.ts
@@ -1,38 +1,27 @@
-import express from "express";
-import { Request, Response, NextFunction } from "express";
-
-import {
-  getAllHotels,
-  createHotel,
-  getHotelById,
-  updateHotel,
-  patchHotel,
-  deleteHotel,
-  getAllHotelsBySearchQuery,
+import { Router } from "express";
+import { 
+    getAllHotels, 
+    createHotel, 
+    getHotelById, 
+    updateHotel, 
+    deleteHotel, 
+    getAllHotelsBySearchQuery,
+    patchHotel,
 } from "../application/hotel";
+import authorizationMiddleware from "./middleware/authorization-middleware";
 import isAuthenticated from "./middleware/authentication-middleware";
-import isAdmin from "./middleware/authorization-middleware";
-import { respondToAIQuery } from "../application/ai";
 
-const hotelsRouter = express.Router();
+const router = Router();
 
-const preMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.method, req.url);
-  next();
-};
-hotelsRouter
-  .route("/")
-  .get(getAllHotels)
-  .post(isAuthenticated, isAdmin, createHotel);
+router.get("/", getAllHotels);
+router.get("/:_id", getHotelById);
+router.get("/search", getAllHotelsBySearchQuery);
 
-hotelsRouter.route("/ai").post(respondToAIQuery);
-hotelsRouter.route("/search").get(getAllHotelsBySearchQuery);
+router.use(isAuthenticated, authorizationMiddleware);
 
-hotelsRouter
-  .route("/:_id")
-  .get(isAuthenticated, getHotelById)
-  .put(updateHotel)
-  .patch(patchHotel)
-  .delete(deleteHotel);
+router.post("/", createHotel);
+router.put("/:_id", updateHotel);
+router.patch("/:_id", patchHotel);
+router.delete("/:_id", deleteHotel);
 
-export default hotelsRouter;
+export default router;
