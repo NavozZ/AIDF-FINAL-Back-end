37f9336b9df818d8ffcd6d7076cabdd32ab999b6
NavozZ <navodyatheshan4@gmail.com>
2025-11-12 06:04:12 +0530

    Refactor hotel API routes: reorganize route definitions for improved readability and maintainability

--- src/api/hotel.ts
+++ src/api/hotel.ts
@@ -4,20 +4,29 @@ import {
     createHotel, 
     getHotelById, 
     updateHotel, 
-    deleteHotel, 
-    getAllHotelsBySearchQuery,
     patchHotel,
+    deleteHotel, 
+    getAllHotelsBySearchQuery, 
 } from "../application/hotel";
 import authorizationMiddleware from "./middleware/authorization-middleware";
 import isAuthenticated from "./middleware/authentication-middleware";
+import { respondToAIQuery } from "../application/ai"; 
 
 const router = Router();
 
-router.get("/", getAllHotels);
-router.get("/:_id", getHotelById);
-router.get("/search", getAllHotelsBySearchQuery);
 
-router.use(isAuthenticated, authorizationMiddleware);
+router.get("/", getAllHotels); 
+
+
+router.post("/ai-search", respondToAIQuery); 
+router.get("/search", getAllHotelsBySearchQuery); 
+
+
+router.get("/:_id", getHotelById); 
+
+
+
+router.use(isAuthenticated, authorizationMiddleware); 
 
 router.post("/", createHotel);
 router.put("/:_id", updateHotel);
